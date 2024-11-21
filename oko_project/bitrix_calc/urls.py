7503533e9a69b67040404348cb51793d60dd0d00
vova045 <vovatsar@bk.ru>
2024-11-21 19:25:49 +0300

    delete company

--- oko_project/bitrix_calc/urls.py
+++ oko_project/bitrix_calc/urls.py
@@ -3,5 +3,4 @@ from . import views
 urlpatterns = [
 path('calculation_list', views.calculation_list, name='calculation_list'),
 path('calculation_add', views.calculation_add, name='calculation_add'),
-path('get_clients', views.get_clients, name='get_clients'),  # Настройте URL для получения клиентов
 ]
--- oko_project/bitrix_calc/views.py
+++ oko_project/bitrix_calc/views.py
@@ -32,35 +32,3 @@ def calculation_add(request):
 
     # Передаем данные в шаблон
     return render(request, "calculation_add.html", {"goods": goods, "grouped_compositions": grouped_compositions})
-
-import requests
-
-import requests
-
-def get_clients_from_php():
-    url = 'https://reklamaoko.ru/static/bitrix_clients.php'
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Проверка на статус ответа
-        return response.json()  # Возвращаем результат в формате JSON
-    except requests.exceptions.RequestException as e:
-        print(f"Ошибка при запросе данных: {e}")
-        raise
-
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-
-@csrf_exempt  # Если необходимо отключить проверку CSRF для этого обработчика
-def get_clients(request):
-    try:
-        clients = get_clients_from_php()  # Получаем данные
-
-        # Логирование полученных данных
-        print("Полученные данные от PHP:", clients)
-
-        if isinstance(clients, list):  # Проверяем, что clients - это список
-            return JsonResponse({"result": clients}, safe=False)
-        else:
-            return JsonResponse({"error": "Ошибка: Ответ не является списком"}, status=400)
-    except Exception as e:
-        return JsonResponse({"error": f"Ошибка при получении данных: {str(e)}"}, status=500)
