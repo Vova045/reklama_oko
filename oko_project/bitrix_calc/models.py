a1deeccacfe73e6865a65069e334df563fbde097
vova045 <vovatsar@bk.ru>
2024-11-27 01:14:39 +0300

    calculation unique false

--- oko_project/bitrix_calc/models.py
+++ oko_project/bitrix_calc/models.py
@@ -63,15 +63,15 @@ class Bitrix_Deal(models.Model):
 class Bitrix_Calculation(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name="Наименование калькуляции")
     goods = models.ForeignKey(Bitrix_Goods, on_delete=models.CASCADE, blank=True, null=True, verbose_name="Товар")
-    price_material = models.CharField(max_length=100, unique=True, verbose_name="Цена за материал")
-    price_add_material = models.CharField(max_length=100, unique=True, verbose_name="Цена за добавочный материал")
-    price_salary = models.CharField(max_length=100, unique=True, verbose_name="Заработная плата")
-    price_payroll = models.CharField(max_length=100, unique=True, verbose_name="Отчисления на заработную плату")
-    price_overheads = models.CharField(max_length=100, unique=True, verbose_name="Накладные расходы")
-    price_cost = models.CharField(max_length=100, unique=True, verbose_name="Себестоимость")
-    price_profit = models.CharField(max_length=100, unique=True, verbose_name="Прибыль")
-    price_salary_fund = models.CharField(max_length=100, unique=True, verbose_name="Фонд заработной платы")
-    price_final_price = models.CharField(max_length=100, unique=True, verbose_name="Общая цена")  # Убрано editable=False
+    price_material = models.CharField(max_length=100, verbose_name="Цена за материал")
+    price_add_material = models.CharField(max_length=100, verbose_name="Цена за добавочный материал")
+    price_salary = models.CharField(max_length=100, verbose_name="Заработная плата")
+    price_payroll = models.CharField(max_length=100, verbose_name="Отчисления на заработную плату")
+    price_overheads = models.CharField(max_length=100, verbose_name="Накладные расходы")
+    price_cost = models.CharField(max_length=100, verbose_name="Себестоимость")
+    price_profit = models.CharField(max_length=100, verbose_name="Прибыль")
+    price_salary_fund = models.CharField(max_length=100, verbose_name="Фонд заработной платы")
+    price_final_price = models.CharField(max_length=100, verbose_name="Общая цена")  # Убрано editable=False
 
 
 class Birtrix_Price_GoodsComposition(models.Model):
