110cebcd945ad15b4cedb76200580ba3141cc9f4
vova045 <vovatsar@bk.ru>
2024-12-04 01:25:27 +0300

    Update urls.py

--- oko_project/bitrix_calc/urls.py
+++ oko_project/bitrix_calc/urls.py
@@ -6,6 +6,6 @@ path('calculation_add', views.calculation_add, name='calculation_add'),
 path('authoritation', views.authoritation, name='authoritation'),
 path('callback/', views.bitrix_callback, name='bitrix_callback'),
 path('api/create-calculation/', views.create_calculation, name='create_calculation'),
-path('api/update-calculation/<int:calculation_id>', views.update_calculation, name='update_calculation'),
+path('api/update-calculation/<int:calculation_id>/', views.update_calculation, name='update_calculation'),
 path('delete-calculation/<int:pk>/', views.delete_calculation, name='delete_calculation'),
 ]
