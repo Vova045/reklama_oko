9879fa61863fbdba822e7fbdbe277a10640bf930
vova045 <vovatsar@bk.ru>
2024-12-04 00:47:31 +0300

    unique false

--- oko_project/bitrix_calc/models.py
+++ oko_project/bitrix_calc/models.py
@@ -78,7 +78,7 @@ class BitrixDeal(models.Model):
 
 
 class Bitrix_Calculation(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name="Наименование калькуляции")
+    name = models.CharField(max_length=100, verbose_name="Наименование калькуляции")
     deal = models.ForeignKey(BitrixDeal, on_delete=models.CASCADE, blank=True, null=True, verbose_name="Сделка")
     goods = models.ForeignKey(Bitrix_Goods, on_delete=models.CASCADE, blank=True, null=True, verbose_name="Товар")
     price_material = models.CharField(max_length=100, verbose_name="Цена за материал")
